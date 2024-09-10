16439efe392f02e17e441a1972fd82a33a638920
nafis200 <nafisahamed14@gmail.com>
2024-09-10 19:08:43 +0600

    width

--- components/tab/Tab.js
+++ components/tab/Tab.js
@@ -4,14 +4,19 @@ import { View, Text,Pressable } from 'react-native'
 import React,{useRef,useState} from 'react'
 import PropTypes from 'prop-types';
 import style from './style';
+import { horizontalScale } from '../../assets/styles/scalling';
 style
 const Tab = (props) => {
  
  const [width,setWidth] = useState(0)
  const textRef = useRef(null)
- console.log(width)
+ const paddingHorizontal = 50
+ const tabWidth = {
+    width: horizontalScale(paddingHorizontal * 3 + width)
+ }
+
   return (
-    <Pressable disabled={props.isInactive} style={[style.tab, props.isInactive && style.inactiveTab]} onPress={()=>props.onPress()}  >
+    <Pressable disabled={props.isInactive} style={[style.tab, props.isInactive && style.inactiveTab,tabWidth]} onPress={()=>props.onPress()}  >
         <Text 
          onTextLayout={(event) => {
             const newWidth = event.nativeEvent.lines[0]?.width || 0;
