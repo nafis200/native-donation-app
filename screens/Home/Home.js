5398c63458996fd7c6a26767e914bcf9d525e37b
nafis200 <nafisahamed14@gmail.com>
2024-09-11 14:46:39 +0600

    userselector

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -12,44 +12,17 @@ import Search from '../../components/search/Search'
 import Button from '../../components/Header/button/Button'
 import SIngleDonationItem from '../../components/singledonetionItem/SIngleDonationItem'
 import { horizontalScale } from '../../assets/styles/scalling'
+import { useSelector } from 'react-redux'
 
 
 
 const Home = () => {
+  const user = useSelector(state => state.user)
+  
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite,globalStyle.flex]}>
-        <Header title={"Azzahri A."} type={1}></Header>
-        {/* <Button title={'Donate'}></Button>
-        <Button title={'Donate'} isDisabled={true} onPress={()=>{
-            console.log("you press me|||")
-        }} ></Button> */}
-       
-        <Search onSearch={(value)=>{
-               console.log(value)
-        }}></Search>
-
-        <View>
-        <Tab title={"highlights"} onPress={()=>{
-           console.log("hellow ")
-        }}></Tab>
-        <Tab title={"LifeStyle"} isInactive={true}></Tab>
-       
-        <SafeAreaView style={{flexDirection:'row',justifyContent:'space-between',paddingHorizontal:horizontalScale(24)}}>
-        <SIngleDonationItem 
-        uri = {"https://i.postimg.cc/mkGsv8Hy/1000-F-767620795-dc-VEg9-TKd-QQ3-PO8km54pc-A5-C1pd-K87l-U.jpg"} 
-        badgeTitle={"Environment"}
-        donationTitle={"tree cactus"}
-        price={44}
-        />
-        <SIngleDonationItem 
-        uri = {"https://i.postimg.cc/mkGsv8Hy/1000-F-767620795-dc-VEg9-TKd-QQ3-PO8km54pc-A5-C1pd-K87l-U.jpg"} 
-        badgeTitle={"Environment"}
-        donationTitle={"tree cactus"}
-        price={44}
-        />
-        </SafeAreaView>
-
-        </View>
+        
+        <Header title={user.firstName + ' ' + user.lastName}></Header>
        
 
     </SafeAreaView>
